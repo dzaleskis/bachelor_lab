eb89a104495222c029b66abfd6d05a3a38880813
Deividas Zaleskis <deividas.zaleskis@devbridge.com>
2022-05-08 15:48:17 +0300

    support counting ops in benchmarks

--- src/benchmarks.hpp
+++ src/benchmarks.hpp
@@ -8,6 +8,7 @@
 #include "classic_algorithms.hpp"
 #include "cycles.hpp"
 #include "statistics.hpp"
+#include "element.hpp"
 
 void bench_int() {
     auto seed = std::time(0);
@@ -15,6 +16,7 @@ void bench_int() {
 
     typedef std::vector<int> (*DistrF)(int, std::mt19937_64&);
     typedef void (*SortF)(std::vector<int>&);
+    typedef void (*ElSortF)(std::vector<Element<int>>&);
 
     std::pair<std::string, DistrF> distributions[] = {
             {"shuffled_int", utils::shuffled_int},
@@ -27,16 +29,16 @@ void bench_int() {
 //            {"push_middle_int", utils::push_middle_int}
     };
 
-    std::pair<std::string, SortF> sorts[] = {
-            {"insertion_sort", &insertion_sort},
-            {"shell_sort_ciura", &ciura_shell_sort},
-            {"shell_sort_tokuda", &tokuda_shell_sort},
+    std::tuple<std::string, SortF, ElSortF> sorts[] = {
+            {"insertion_sort", &insertion_sort, &insertion_sort},
+            {"shell_sort_ciura", &ciura_shell_sort, &ciura_shell_sort},
+            {"shell_sort_tokuda", &tokuda_shell_sort, &tokuda_shell_sort},
 //            {"standard_sort", &standard_sort},
     };
 
     int sizes[] = {64, 256 };
     const int bench_ms = 5000;
-    const int warmup_ms = bench_ms / 2;
+    const int warmup_ms = 1000;
 
     for (auto& distribution : distributions) {
         for (auto& sort: sorts) {
@@ -49,27 +51,40 @@ void bench_int() {
                 while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - warmup_start).count() < warmup_ms) {
                     std::vector<int> v = distribution.second(size, el);
 
-                    sort.second(v);
+                    std::get<1>(sort)(v);
                 }
 
                 std::chrono::time_point<std::chrono::steady_clock> total_start = std::chrono::steady_clock::now();
+                std::vector<int> comparisons;
+                std::vector<int> assignments;
                 std::vector<uint64_t> cycles;
 
                 // run the benchmark for `bench_ms`
                 while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - total_start).count() < bench_ms) {
                     std::vector<int> v = distribution.second(size, el);
+                    std::vector<Element<int>> e(v.begin(), v.end());
 
-                    uint64_t start = rdtsc();
-                    sort.second(v);
-                    uint64_t end = rdtsc();
+                    auto c = measure_cycles([&]() {
+                        std::get<1>(sort)(v);
+                    });
 
-                    cycles.push_back(end - start);
+                    auto report = global_measure.withReport([&]() {
+                        std::get<2>(sort)(e);
+                    });
+
+                    comparisons.push_back(report.comparisons);
+                    assignments.push_back(report.assignments);
+                    cycles.push_back(c);
                 }
 
+                auto cmp_stats = calculate_statistics(comparisons);
+                auto assign_stats = calculate_statistics(assignments);
                 auto cycles_stats = calculate_statistics(cycles);
 
-                std::cerr << size << " " << distribution.first << " " << sort.first
-                    << " " << cycles_stats.median << " " << cycles_stats.iqr << " " << cycles_stats.outliers <<"\n";
+                std::cerr << size << " " << distribution.first << " " << std::get<0>(sort)
+                    << " cycles: " << cycles_stats.median << " " << cycles_stats.iqr << " " << cycles_stats.outliers
+                    << " comparisons: " << cmp_stats.median << " " << cmp_stats.iqr << " " << cmp_stats.outliers
+                    << " assignments: " << assign_stats.median << " " << assign_stats.iqr << " " << assign_stats.outliers <<"\n";
             }
         }
     }
